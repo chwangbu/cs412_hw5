37ebefd8f381fa33aad97782e3ac90ee692b26d5
chwangbu <bryho@bu.edu>
2024-10-17 19:15:44 -0400

    added view to handle status message (forgot to save

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -3,6 +3,7 @@ from .views import ShowProfiles
 from .views import ShowProfilePageView
 from .views import CreateProfileView
 
+
 urlpatterns = [
     path('', ShowProfiles.as_view(), name='show_all_profiles'),
     path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'),
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 from django.views.generic import CreateView
 from .forms import CreateProfileForm
 from .models import Profile
+from .forms import CreateStatusMessageForm
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class ShowProfiles(ListView):
@@ -21,6 +23,10 @@ class CreateProfileView(CreateView):
     model = Profile
     form_class = CreateProfileForm
     template_name = 'mini_fb/create_profile_form.html'
+
+    def form_valid(self, form):
+        form.instance.profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('show_profile', kwargs={'pk': self.object.pk})
