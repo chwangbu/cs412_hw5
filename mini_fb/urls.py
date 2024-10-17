f8bf946f3088b8305ec34146c2a17c155c178eda
chwangbu <bryho@bu.edu>
2024-10-17 18:31:01 -0400

    Created CreateStatusMessageForm

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import ShowProfiles
 from .views import ShowProfilePageView
+from .views import CreateProfileView
 
 urlpatterns = [
     path('', ShowProfiles.as_view(), name='show_profiles'),
     path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'),
+    path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
 ]
