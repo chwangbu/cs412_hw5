bee494a83835c730b8c9bcf46fb1a1371bd4c8ca
chwangbu <bryho@bu.edu>
2024-10-17 18:58:45 -0400

    Debugging because links were not working

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -4,7 +4,7 @@ from .views import ShowProfilePageView
 from .views import CreateProfileView
 
 urlpatterns = [
-    path('', ShowProfiles.as_view(), name='show_profiles'),
+    path('', ShowProfiles.as_view(), name='show_all_profiles'),
     path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'),
     path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
 ]
