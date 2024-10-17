b5f6fd1da031d02af8cd8def7dfaf4fedda16751
chwangbu <bryho@bu.edu>
2024-10-17 16:42:39 -0400

    Created new class profile for ShowProfilePageView

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from .models import Profile
 
 # Create your views here.
 class ShowProfiles(ListView):
     model = Profile
-    template_name = 'mini_fb/show_profiles.html'
+    template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'mini_fb/show_profile.html'
