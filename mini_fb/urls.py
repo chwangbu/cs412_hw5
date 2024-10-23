d6d6841e2857f4ccfe21bfe8bd2b1aa4fd17b4d7
chwangbu <bryho@bu.edu>
2024-10-23 16:08:59 -0400

    Created UpdateProfileForm, Created UpdateProfileView, Created update_profile_form.html, Added URL path

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -5,13 +5,14 @@ from .views import ShowProfiles
 from .views import ShowProfilePageView
 from .views import CreateProfileView
 from .views import CreateStatusMessageView
-
+from .views import UpdateProfileView
 
 urlpatterns = [
     path('', ShowProfiles.as_view(), name='show_all_profiles'),
     path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'),
     path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
     path('profile/<int:pk>/create_status/', CreateStatusMessageView.as_view(), name='create_status'),
+    path('profile/<int:pk>/update', UpdateProfileView.as_view(), name='update_profile'),
 ]
 
 if settings.DEBUG:
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -4,9 +4,11 @@ from django.views.generic import DetailView
 from django.urls import reverse
 from django.views.generic import CreateView
 from .forms import CreateProfileForm
-from .models import Profile, StatusMessage
+from .models import Profile, StatusMessage, Image
 from .forms import CreateStatusMessageForm
 from django.shortcuts import get_object_or_404
+from django.views.generic.edit import UpdateView
+from .forms import UpdateProfileForm
 
 # Create your views here.
 class ShowProfiles(ListView):
@@ -34,7 +36,22 @@ class CreateStatusMessageView(CreateView):
 
     def form_valid(self, form):
         form.instance.profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        sm = form.save()
+
+        files = self.request.FILES.getlist('files')
+        for fi in files:
+            image = Image(image_file = fi, status_message=sm)
+            image.save()
+
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('show_profile', kwargs={'pk': self.kwargs['pk']})
+    
+class UpdateProfileView(UpdateView):
+    model = Profile
+    form_class = UpdateProfileForm
+    template_name = 'mini_fb/update_profile_form.html'
+
+    def get_success_url(self):
+        return reverse('show_profile', kwargs={'pk': self.object.pk})
