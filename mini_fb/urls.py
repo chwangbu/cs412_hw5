1d7b9f3f719229484a2e7ad0a13086f9b312b6e9
chwangbu <bryho@bu.edu>
2024-10-28 19:35:12 -0400

    Added URL for news feed

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -8,6 +8,7 @@ from .views import CreateStatusMessageView
 from .views import UpdateProfileView
 from .views import UpdateStatusMessageView, DeleteStatusMessageView
 from .views import CreateFriendView
+from .views import ShowNewsFeedView
 
 urlpatterns = [
     path('', ShowProfiles.as_view(), name='show_all_profiles'),
@@ -18,6 +19,7 @@ urlpatterns = [
     path('status/<int:pk>/update', UpdateStatusMessageView.as_view(), name='update_status'),
     path('status/<int:pk>/delete', DeleteStatusMessageView.as_view(), name='delete_status'),
     path('profile/<int:pk>/add_friend/<int:other_pk>/', CreateFriendView.as_view(), name='add_friend'),
+    path('profile/<int:pk>/news_feed', ShowNewsFeedView.as_view(), name='news_feed')
 ]
 
 if settings.DEBUG:
