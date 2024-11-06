706ffba45e1ac205b5a31c5d3bd8858d5c697a0f
chwangbu <bryho@bu.edu>
2024-11-06 18:47:03 -0500

    Debugging:Added null/blank parameter in the user field

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     city = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     profile_image_url = models.CharField(max_length=150)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
