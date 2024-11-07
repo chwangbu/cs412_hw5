d256177cd7a28a8c15bf16721b5dcf3c01b4999f
chwangbu <bryho@bu.edu>
2024-11-06 19:12:51 -0500

    Debugging: Wrong section of code debugged

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -15,8 +15,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', ShowProfiles.as_view(), name='show_all_profiles'),
     path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'),
-    path('profile/create_profile/', CreateProfileView.as_view(), name='create_profile'),
-    path('profile/create_status/', CreateStatusMessageView.as_view(), name='create_status'),
+    path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
+    path('status/create_status/', CreateStatusMessageView.as_view(), name='create_status'),
     path('profile/update/', UpdateProfileView.as_view(), name='update_profile'),
     path('status/<int:pk>/update', UpdateStatusMessageView.as_view(), name='update_status'),
     path('status/<int:pk>/delete', DeleteStatusMessageView.as_view(), name='delete_status'),
